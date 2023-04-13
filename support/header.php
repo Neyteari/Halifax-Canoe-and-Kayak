28a004b7460f6e255af19438a25d03c4d40f3c53
Neyteari <126405651+Neyteari@users.noreply.github.com>
2023-04-13 18:07:51 -0300

    Update header and footer

---
+++ support/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halifax Canoe and Kayak</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header class="header_bar">
+        <img id="nav_toggle_button" class="header_button" src="images/hamburger.png" alt="Menu">
+        <img class="logo" src="images/paddle-white.png" alt="logo">
+        <nav class="header_nav">
+            <ul class="header_nav_menu">
+                <li>Home</li>
+                <li>Book a Trip</li>
+                <li>Admin Login</li>
+            </ul>
+        </nav>
+        <div class="title"><h1>Halifax Canoe and Kayak</h1></div>
+        </header>       
+    </header>
+    <div class="head_image">
+        <img src="images/canoe.jpg" alt="Come experience Canada">
+        <div class="centered">Come Experience<br/>Canada</div>
+    </div>
+    <div>
+        <h2>Upcoming Adventures</h2>
+    </div>
+    <hr/>
+    <div>
+        <h3><a href="">Halifax</a></h3>
+        <p>Date: 2017-04-12<br/>Duration: 4 days</p>
+        
+        <h4>Summary</h4>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <h3><a href="">Sydney</a></h3>
+        <p>Date: 2017-05-10<br/>Duration: 4 days</p>
+            
+        <h4>Summary</h4>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+    </div>
