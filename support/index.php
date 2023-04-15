b3f0bbcf39ee8c2a5b10378f0143029dfdd51670
Neyteari <126405651+Neyteari@users.noreply.github.com>
2023-04-15 19:40:00 -0300

    Add initial form

--- support/index.php
+++ support/index.php
@@ -1,5 +1,25 @@
 <?php
-include ('header.php');
+include ('header.php'); 
+?>
 
+<div>
+    <form action="problem.php" method="POST">
+    <p>Title: <select name="title">
+        <option value="Mr">Mr.</option>
+        <option value="Ms">Ms.</option>
+        <option value="Mrs">Mrs.</option>
+    </select></p>
+    <p>First Name: <input type="text" name="first" /></p>
+    <p>Last Name: <input type="text" name="last" /></p>
+    <p>Role: <select name="role">
+        <option value="Admin">Admin</option>
+        <option value="Manager">Manager</option>
+        <option value="CEO">CEO</option>
+    </select>
+    <input type="submit" name="submit" value="Submit" />
+    </p>
+    </form>
+</div>
+<?php
 include ('footer.php');
 ?>
