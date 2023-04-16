2e96a89b5fd5f4c0305c8857684cd9a11bd9ac90
Neyteari <126405651+Neyteari@users.noreply.github.com>
2023-04-16 18:03:20 -0300

    Refactor problem page

--- support/problem.php
+++ support/problem.php
@@ -1,35 +1,38 @@
 <?php
-include('header.php');
-
+$body = '';
 if (isset($_POST['submit']))
 {
     $role = $_POST['role'];
 
+    $body .= '<h1>Hello ' . $role . '</h1><hr/>';
+    $body .= '<h2>Here are your options:</h2>';
+
     if ($role == 'Admin')
     {
-        //code
-
+        $_SESSION['role'] = 'Admin';
+        $body .= '<a href="new-account.php">New Account</a><br/>'; 
     }
     elseif ($role == 'Manager')
     {
-
+        $_SESSION['role'] = 'Manager';
+        $body .= '<a href="lost-password.php">Lost Password</a><br/>';
     }
     elseif ($role == 'CEO')
     {
-
-    }
-    else
-    {
-        echo 'Please go <a href="index.php">here</a> to re-submit the form';
+        $_SESSION['role'] = 'CEO';
+        $body .= '<a href="need-help.php">Phone Number</a><br/>';
     }
 
-    echo '<h1>Hello ' . $role . '</h1><hr/>';
-    echo '<h2>Here are your options:</h2>';
+    $body .= '<a href="isnt-working.php">My Computer isn\'t working</a>';
 }
 else 
 {
-    echo 'Please go <a href="index.php">here</a> to submit the form';
+    $body .= 'Please go <a href="index.php">here</a> to submit the form';
 }
+
+include('header.php');
+
+echo $body;
 
 include('footer.php');
 ?>
